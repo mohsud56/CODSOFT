5d59c573a9c9ec607deaa1835814ccc84c9f5928
mohsud56 <mohsud56@gmail.com>
2025-04-12 13:24:00 +0200

    Update to_do_list.py

--- Task 4/to_do_list.py
+++ Task 4/to_do_list.py
@@ -39,7 +39,7 @@ def main():
             break
 
         else:
-            print("Invalid choice. ")
+            print("Invalid choice. Please try again from (1-4). ")
 
 if __name__ == "__main__":
     main()
